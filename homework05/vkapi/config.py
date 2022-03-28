ccbbeaf9d409aed4287b06c779c3c2928fb1554e
Alisa-Mikulina <alice.mikulina@gmail.com>
2022-03-28 21:55:39 +0300

    dubl 2

--- homework05/vkapi/config.py
+++
@@ -1,8 +0,0 @@
-# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
-
-VK_CONFIG = {
-    "domain": "https://api.vk.com/method",
-    "client_id": "8080667",
-    "access_token": "2f426492bf71587252f6552f867a712623e24b44363439c700d4189ec241f733029c0a70d03954e6217af",
-    "version": "5.126",
-}
