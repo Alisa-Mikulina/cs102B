07622172094737c4fd42c97f6d041f62221f89a7
Alisa-Mikulina <alice.mikulina@gmail.com>
2022-03-28 22:38:07 +0300

    dubl 5

--- homework05_sem2/vkapi/wall.py
+++ homework05_sem2/vkapi/wall.py
@@ -5,7 +5,6 @@ from string import Template
 
 import pandas as pd
 from pandas import json_normalize
-
 from vkapi import Session, config
 from vkapi.config import VK_CONFIG
 from vkapi.exceptions import APIError
